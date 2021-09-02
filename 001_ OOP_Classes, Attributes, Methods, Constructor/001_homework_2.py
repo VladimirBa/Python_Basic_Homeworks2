9cc17d5be4ab5952cac8aa85a12b9e0409d3e1d6
Vladimir <vldmr510@gmail.com>
2021-09-02 15:19:50 +0300

    Methods __init__, __str__ have been added.

--- 001_ OOP_Classes, Attributes, Methods, Constructor/001_homework_2.py
+++ 001_ OOP_Classes, Attributes, Methods, Constructor/001_homework_2.py
@@ -6,35 +6,50 @@ class Book:
         self.year = year
         self.genre = genre
 
+    def __str__(self):
+        return f'This book "{self.title}" of the {self.genre} genre were written by {self.author} in {self.year}.'
+
 
 class Resume:
 
-    resumeCD = """          “The Memoirs of Sherlock Holmes” was initially published in 1894 after each of 
+    def __init__(self, resume):
+        self.resume = resume
+
+    def get_resume(self):
+        if book1:
+            return self.resume == resume1
+        else:
+            return self.resume == resume2
+
+    def __str__(self):
+        return self.resume
+
+
+resume_cd = '''          “The Memoirs of Sherlock Holmes” was initially published in 1894 after each of 
     the individual stories contained within had appeared separately in The Strand magazine. This collection
     was the follow-up to “The Adventures of Sherlock Holmes” which introduced the detective
     and his loyal companion Dr. John Watson. The Memoirs are generally considered the superior
     to the collection which introduced the detective, but not as strong as the stories he solved
     in the next collection following his resurrection. The Memoirs of Sherlock Holmes
     draws to a close with Dr. John Watson disconsolate in his conviction that Holmes and
-    his nemesis Prof. Moriarty fell to their deaths after a struggle atop Reichenbach Falls."""
+    his nemesis Prof. Moriarty fell to their deaths after a struggle atop Reichenbach Falls."'''
 
-    resumeSK = """          “Bag of Bones” is a 1998 fantasy novel by American author Stephen King.
+resume_sk = '''          “Bag of Bones” is a 1998 fantasy novel by American author Stephen King.
     Set in the fictional unincorporated town of TR-90, Maine, it follows an author,
     Mike Noonan, who suffers from writer’s block. Having isolated himself at his lake house to write,
     Noonan begins to experience delusions linked to the untimely death of his wife four years earlier.
     The plot borrows some of its tensions, events, and characterizations from Rebecca,
     a Gothic novel by English author Daphne du Maurier. Though not his most famous work,
     Bag of Bones is considered one of King’s best written and has been the recipient of many awards,
-    including the British Fantasy Award for Best Novel and the Bram Stoker Award."""
-
+    including the British Fantasy Award for Best Novel and the Bram Stoker Award."'''
 
 book1 = Book(author='Conan Doyle', title='THE MEMOIRS OF SHERLOCK HOLMES', year=1894, genre='detective')
 book2 = Book(author='Stephen King', title="BAG OF BONES", year=1998, genre='fantasy')
 
-print(f'This book {book1.title}  of {book1.genre} genre were written by {book1.author} in {book1.year}.',
-      Resume.resumeCD, sep='\n\n')
+resume1 = Resume(resume_cd)
+resume2 = Resume(resume_sk)
 
-print('****************************************')
-
-print(f'This book {book2.title}  of {book2.genre} genre were written by {book2.author} in {book2.year}.',
-      Resume.resumeSK, sep='\n\n')
+print(book1, resume1, sep='\n\n')
+print(' * '*20)
+print()
+print(book2, resume2, sep='\n\n')
