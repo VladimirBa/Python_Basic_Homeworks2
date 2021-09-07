c8143fef9047bea4dc6af58b0059dfd1f8c0a708
Vladimir <vldmr510@gmail.com>
2021-09-07 11:58:25 +0300

    003_homework_2.py - homework has been added.

--- 003_OOP_Encapsulation_Polymorphism_Abstraction/003_OOP_homework_1.py
+++ 003_OOP_Encapsulation_Polymorphism_Abstraction/003_OOP_homework_1.py
@@ -1,30 +1,30 @@
 class Editor:
 
-    @staticmethod
-    def view_document():
+    def view_document(self):
         print('You may only view your text in the free version of the Text Editor Pro.')
 
-    @staticmethod
-    def edit_document():
+    def edit_document(self):
         print('You have to pay to edit your text in the Text Editor Pro.')
 
 
-class ProEditor:
+class ProEditor(Editor):
 
-    @staticmethod
-    def edit_document(lic_key):
-        while lic_key != '1357':
-            parent_class_obj = Editor()
-            parent_class_obj.edit_document()
-            return "Sorry. You have entered an incorrect license code."
-        else:
-            ProEditor()
-            return 'You are permitted to edit your text!'
+    def edit_document(self):
+        print('You are permitted to edit your text!')
 
 
-start = Editor()
-start.view_document()
-start.edit_document()
-text = ProEditor()
-lk = input('Please, enter the license code you received by email after payment: ')
-print(text.edit_document(lic_key=lk))
+def main():
+    editor = Editor()
+    editor.view_document()
+    editor.edit_document()
+    proeditor = ProEditor()
+
+    lk = input('Please, enter the license code you received by email after payment: ')
+    if lk == '1357':
+        proeditor.edit_document()
+    else:
+        print("Sorry. You have entered an incorrect license code.")
+
+
+if __name__ == '__main__':
+    main()
