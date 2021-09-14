a326e12b25d6fb46a77d27de4ed917a2a19ce1c3
Vladimir <vldmr510@gmail.com>
2021-09-14 11:04:46 +0300

    005_homework_adnl.py - additional homework has been added.

---
+++ 005_Iterations/005_homework_1.py
@@ -0,0 +1,32 @@
+class ReversedList(object):
+
+    def __init__(self, my_list):
+
+        self.my_list = my_list
+        self.index = len(my_list) - 1
+
+    def __iter__(self):
+
+        return self
+
+    def __next__(self):
+
+        if self.index < 0:
+            raise StopIteration
+
+        next_elem = self.my_list[self.index]
+        self.index -= 1
+
+        return next_elem
+
+
+def main():
+    my_list = ['a', 1, 'z', -9, 'A', 10]
+    list_print = []
+    for i in ReversedList(my_list):
+        list_print.append(str(i))
+    print(*list_print, sep=', ')
+
+
+if __name__ == '__main__':
+    main()
